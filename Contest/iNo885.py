254ef09c3b0389e587f9c4399e68f432df5bdc5b
kiki <32135666+kikihiter@users.noreply.github.com>
2018-08-06 19:12:46 +0800

    Create iNo885.py

---
+++ Contest/iNo885.py
@@ -0,0 +1,13 @@
+"""
+input:[3,3,4,4,4,6,8]
+      16
+output:2
+"""
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        """
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        #people
