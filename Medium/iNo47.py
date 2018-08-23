63c8d78e7075ee0506dd82dc38bb9b594d8b8822
kiki <32135666+kikihiter@users.noreply.github.com>
2018-08-23 21:48:39 +0800

    Create iNo47.py

---
+++ Medium/iNo47.py
@@ -0,0 +1,42 @@
+#直接用上一题的结果了,用上一题的思想的话，完全没有变化
+#之前写的，查找下一个字典序的算法
+def nextPer(num):
+    #之前写的算法是直接在原列表上进行修改，并没有使用额外辅助空间，导致最终结果perList中元素全部指向同一位置，在这里进行修改，新生成一个列表并进行返回。
+    nums = []
+    for yuansu in num:
+        nums.append(yuansu)
+    
+    for i in range(len(nums)-1,0,-1):
+        #从后向前寻找第一个非逆序数对
+        if nums[i] > nums[i-1]:
+            #一个中间变量，为比nums[i-1]大的数
+            temp = nums[i]
+            #在i后面寻找比nums[i-1]大的数中的最小的数值
+            for j in range(len(nums)-1,i,-1):
+                if nums[j] > nums[i-1]:
+                    temp = min(temp,nums[j])
+            #找到这个数的序号，应当注意，应该从后往前找到这个序号，不能直接从前开始找，因为可能前方有相同的值的元素
+            iIndex = len(nums) - nums[::-1].index(temp) - 1                        
+            nums[i-1],nums[iIndex] = nums[iIndex],nums[i-1]
+            nums[i:] = sorted(nums[i:])
+            break
+    else:
+        nums[:] = sorted(nums[:])
+    return nums
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        perList = []
+        nums[:] = sorted(nums[:]) 
+        perList.append(nums)
+        nums = nextPer(nums)
+        while nums != sorted(nums):
+            perList.append(nums)
+            nums = nextPer(nums)
+            #print nums
+
+        return perList
